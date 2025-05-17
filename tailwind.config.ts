bffbdf84c78db97e6c62cc0f8869e3e8f1865508
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-05-17 14:22:40 -0500

    Add tailwind config in the project

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000000",
+          100: "#000319",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
