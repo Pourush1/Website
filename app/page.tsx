f0aa960f4e34e73bc17ee0be45f9fc109b2ff7b3
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-05-19 21:48:45 -0500

    Update dependencies and enhance styles; add clsx, motion, and tailwind-merge packages, and implement spotlight animation in global styles.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100">
       <div className="max-w-7xl w-full">
         <h1 className="">Hello World</h1>
+        <Hero />
       </div>
     </main>
   );
