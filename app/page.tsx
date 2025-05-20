0e53f7308c75e9471f87bafdff88a6df02848e02
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-05-19 23:14:05 -0500

    Implement spotlight animation in Tailwind configuration using v3. Needed to make some changes to make it compatibe with v3

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100">
       <div className="max-w-7xl w-full">
-        <h1 className="">Hello World</h1>
+        <h1 className="text-4xl font-bold underline">Hello World</h1>
         <Hero />
       </div>
     </main>
