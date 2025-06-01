3d8040b0f75b27c45b41bf214d7e77e566348cde
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-05-31 21:21:12 -0500

    Update Home component layout to center content and adjust padding for improved responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-4xl font-bold underline">Hello World</h1>
         <Hero />
       </div>
     </main>
