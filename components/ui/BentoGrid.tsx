64151413ec042def184a0e3e90c1b7c5d0eb8ce8
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-06-09 19:31:02 -0500

    Fix class name formatting in BentoGrid and update grid item titles and class names for consistency and clarity.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -157,7 +157,7 @@ export const BentoGridItem = ({
           )}
 
           {id === 6 && (
-            <div className="mt=5 relative">
+            <div className="mt-5 relative">
               <div className="absolute -bottom-5 right-0">
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
