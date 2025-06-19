4f3b7c2915c277112ac937ee2cc39391346bd51d
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-06-18 21:51:30 -0500

    Update Hero component text for personalization and enhance Pin component to open links in a new tab for improved user experience.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Portfolio with Next.js
+            Pourush Shrestha Portfolio
           </p>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -40,8 +40,8 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            A Software Engineer who loves solving real world problems with my
-            software engineering knowledge.
+            A Software Engineer passionate about delivering innovative solutions
+            to complex challenges through cutting-edge technology and expertise.
           </p>
 
           <a href="#about">
