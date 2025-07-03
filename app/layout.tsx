4c8b0bae7a22bc1f5e0bea09a3e8afcdf763655b
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-07-02 19:42:18 -0500

    Remove Sentry configuration files and related components to streamline project structure and eliminate redundancy in Sentry initialization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import FeedbackButton from "@/components/FeedbackButton";
-import SentryInit from "@/components/SentryInit";
-import { ThemeProvider } from "./provider";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from './provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Pourush Portfolio",
-  description: "Modern Portfolio made by Pourush",
+  title: 'Pourush Portfolio',
+  description: 'Modern Portfolio made by Pourush',
 };
 
 export default function RootLayout({
@@ -21,10 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`${inter.className}`}>
-        <SentryInit />
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
-          <FeedbackButton />
         </ThemeProvider>
       </body>
     </html>
